b4afdf19ab72b6aad78d96b0fe561ad8cf411ef9
MigiMoore <carmichaelkw47@gmail.com>
2020-01-02 21:56:08 -0500

    fixed input box bug by adding a textAlign style to the input box in JSX , fixed the key presses  by taking off the quotes from the number codes

--- src/calculator.js
+++ src/calculator.js
@@ -1,17 +1,21 @@
-//12/31/19 
+//1/2/20
 import React from 'react';
-// program deciaml restriction logic 
-// program parsenthesis buttons 
+
+
 // program = operator button 
+// WHEN BROWSER RESIZES BUTTONS SHRINK .add responsivenes to calculator  
 // as you enter numbers into fragment expression make placement commas 
 // program expression history display 
 // program  % , Sqrt, x^3, X^2, 
 // program a !! ANOTHER COMPONENT solution history section
 // add REDUX to your app
 // connect to an APi in Your APP
+// program parsenthesis buttons 
 // make it look pretty
 
-
+let style2 ={
+        textAlign:'right'
+    };
 class Calculator extends React.Component{
 	constructor(props){
 		super(props);
@@ -26,14 +30,17 @@ class Calculator extends React.Component{
         }
 
     	numberDisplay = (input) => {
-    		//decimal isnt placed at the end of the string. 
-            //decimal  replaces "0" at the beginning of the state.
-            if(this.state.expFrag.includes('.') == true && input === '.'){
+            if(this.state.expFrag.includes('.') === true && input === '.'){
                 return;
             }else{
         		if( this.state.expFrag === '0' && input === '0'){
         			return;
-        		}else if (this.state.expFrag === '0' && input != '0'){
+        		}else if (this.state.expFrag === '0' && input === '.'){
+                    this.setState({
+                        expFrag:this.state.expFrag.concat(input),
+                        expression:this.state.expression
+                    });
+                }else if (this.state.expFrag === '0' && input != '0'){
         			this.setState({
         				expFrag:input,
         				expression:this.state.expression
@@ -46,6 +53,12 @@ class Calculator extends React.Component{
         		};
             };
     	};
+
+        appender = (num) =>{
+            let exp = this.state.expFrag;
+            return setTimeout(num =>exp.concat(num),10);
+            //(this.state.expFrag.includes('.') is not a function
+        }
    
     	expAppend = () => {
     		this.setState({
@@ -84,7 +97,7 @@ class Calculator extends React.Component{
     //deletes the character at the end of the state display string
     backspace = () =>{
     	//slice portion and set
-    	if( this.state.expFrag.length == 1){
+    	if( this.state.expFrag.length === 1){
     		this.setState({
     			expFrag:'0'
     		});
@@ -99,7 +112,7 @@ class Calculator extends React.Component{
     sign = () => {
         let exp = [...this.state.expFrag]; 
         if(exp.includes("-") === false){
-            exp.push("-"); 
+            exp.splice(0,0,'-'); 
             this.setState({
                 expFrag: exp.join(""),
                 expression: this.state.expression
@@ -117,86 +130,82 @@ class Calculator extends React.Component{
         window.addEventListener(
             "keydown", 
             (key) =>{ 
-                if(key.keyCode == "97"){
+                if(key.keyCode === 97){
                     this.numberDisplay('1');
-                }else if (key.keyCode == "98"){
+                }else if (key.keyCode === 98){
                     this.numberDisplay('2');
-                }else if (key.keyCode == "99"){
+                }else if (key.keyCode === 99){
                     this.numberDisplay('3');
-                }else if (key.keyCode == "100"){
+                }else if (key.keyCode === 100){
                     this.numberDisplay('4');
-                }else if (key.keyCode == "101"){
+                }else if (key.keyCode === 101){
                     this.numberDisplay('5');
-                }else if (key.keyCode == "102"){
+                }else if (key.keyCode === 102){
                     this.numberDisplay('6');
-                }else if (key.keyCode == "103"){
+                }else if (key.keyCode === 103){
                     this.numberDisplay('7');
-                }else if (key.keyCode == 104){
+                }else if (key.keyCode === 104){
                     this.numberDisplay('8');
-                }else if (key.keyCode == "105"){
+                }else if (key.keyCode === 105){
                     this.numberDisplay('9');
-                }else if (key.keyCode == "104"){
+                }else if (key.keyCode === 104){
                     this.numberDisplay('8');
-                }else if (key.keyCode == "8"){
-                    this.backspace();
-                }else if (key.keyCode == "110"){
-                    this.numberDisplay('.');
-                }else if (key.keyCode == "111"){
-                    this.appendReset('/');
-                }else if (key.keyCode == "106"){
-                    this.appendReset('*');
-                }else if (key.keyCode == 109){
-                    this.appendReset('-');
-                }else if (key.keycode == 32){
-                    this.clearEntry();
-                }else if (key.keycode == '96' || key.keycode == '45'){
+                }else if (key.keyCode === 96){
                     this.numberDisplay('0');
-                }else  {
-                    this.appendReset('+');
+                }else if (key.keyCode === 8){
+                    this.backspace();
+                }else if (key.keyCode === 110){
+                    this.numberDisplay('.');
+                }else if (key.keyCode === 111){
+                    this.appendReset('/');
+                }else if (key.keyCode === 106){
+                    this.appendReset('*');
+                }else if (key.keyCode === 109){
+                    this.appendReset('-');
+                }else if (key.keycode === 32){
+                    this.clearEntry();
                 }
         },false);
         
     };
     
+    
+        
 	render(){
 		return(
 	   <div>
 			 <h1>Calculator</h1>
 	         <div id="parent">
-		          <input id="display" dir="rtl" value={this.state.expFrag}/><br/>
-		          <div class="b">
+		          <input id="display" style={style2} value={this.state.expFrag}/><br/>
+		          <div className="b">
 		            <button id="clearEntry" onClick={ () => this.clearEntry()}>CE</button>
 		            <button id="allClear" onClick={ () => this.clear()}>C</button>
 		            <button id="backspace" onClick={() => this.backspace()}>bck</button>
 		            <button id="division"onClick={() => this.appendReset('/')}>/</button>
 		         </div>
-		         <div class="b">
+		         <div className="b">
 		            <button id="seven" onClick ={() => this.numberDisplay('7')}>7</button> 
 		            <button id="eight" onClick ={() => this.numberDisplay('8')}>8</button>
 		            <button id="nine" onClick ={() => this.numberDisplay('9')}>9</button>
 		            <button id="multiply" onClick ={ () => this.appendReset('X')}>X</button>
 		         </div>
-		         <div class="b">
+		         <div className="b">
 		            <button id="six" onClick ={() => this.numberDisplay('6')}>6</button>
 		            <button id="five" onClick ={() => this.numberDisplay('5')}>5</button>
 		            <button id="four" onClick ={() => this.numberDisplay('4')}>4</button>
 		            <button id="subtract" onClick ={() => this.appendReset('-')}>-</button>
 		         </div>
-		         <div class="b">
+		         <div className="b">
 		            <button id="one" onClick ={() => this.numberDisplay('1')}>1</button>
 		            <button id="two" onClick ={() => this.numberDisplay('2')}>2</button>
 		            <button id="three" onClick ={() => this.numberDisplay('3')}>3</button>
 		            <button id="add" onClick ={() => this.appendReset('+')}>+</button>
 		         </div>
-		         <div class="b">
+		         <div className="b">
 		            <button id="sign" onClick ={() => this.sign()}>+/-</button>
 		            <button id="zero" onClick ={() => this.numberDisplay('0')}>0</button>
 		            <button id="decimal" onClick ={() => this.numberDisplay('.')}>.</button>
 		            <button id="result">=</button>
-		         </div>
-		         <div class="b">
-		          <button id="leftParenthesis">(</button>
-		          <button id="rightParenthesis">)</button>
 		         </div>
 	        </div>
         </div>);
@@ -205,51 +214,47 @@ class Calculator extends React.Component{
 
 export default Calculator;
 
-
-
-
-
-
-
-
-// GRAVEYARD FOR OLD numberDisplay
-
-/*function called in each input buttons onClick event handler. It receives as an arguement the number correspoding to each button. 
-    	if an arithmetic character is the last character in the state string, another following arithmetic 
-    	character cannot be placed after.*/
-   		/*if state == 0 set desplay prop to input (to replace). other wise concat input 
-	    dont allow the user to repeat 0's ie 00+8*/
-// let display = [...this.state.display] , decimalCheck = [...this.state.decimalCheck];
-//     		let target = display[display.length - 1];
-//     		let regex = /\D/;
-//     		// let nonDigitExceptDecimal = /\d^./
-//     		if( this.state.display == '0'){
-//     			if(/\d/.test(input)){
-//     				this.setState({
-//     					display: input.toString() ,
-//     					decimalCheck: input.toString()
-
-//     				})
-//     			}
-//     			else{
-//     				this.setState({
-//     					display: this.state.display.concat(input) ,
-//     					decimalCheck: this.state.decimalCheck.concat(input)
-//     				})
-//     			}
-//     			//if a decimal exist in input string dont allow the decimal button to push a decimal into the state. 
-//     		    //if an arithmetic operator gets inputted only check wht comes after the operator and when anothr gets entered clear the array
-//     		}else{
-//     			if((regex.test(target)=== true) && (regex.test(input)=== true)){
-// 	    			return;
-// 	    		}else if( decimalCheck.includes('.') && input === '.'){
-// 	    			return;
-// 	    		}else{
-// 			    		this.setState({
-// 			    			display: this.state.display.concat(input) ,
-// 			    			decimalCheck: this.state.decimalCheck
-// 		    		})
-// 		    	}
-//     		}
-
-
+// keypress = () => {
+//         window.addEventListener(
+//             "keydown", 
+//             (key) =>{ 
+//                 if(key.keyCode === "97"){
+//                     this.numberDisplay('1');
+//                 // }else if (key.keyCode === 98){
+//                 //     this.numberDisplay('2');
+//                 // }else if (key.keyCode === 99){
+//                 //     this.numberDisplay('3');
+//                 // }else if (key.keyCode === 100){
+//                 //     this.numberDisplay('4');
+//                 // }else if (key.keyCode === 101){
+//                 //     this.numberDisplay('5');
+//                 // }else if (key.keyCode === 102){
+//                 //     this.numberDisplay('6');
+//                 // }else if (key.keyCode === 103){
+//                 //     this.numberDisplay('7');
+//                 // }else if (key.keyCode === 104){
+//                 //     this.numberDisplay('8');
+//                 // }else if (key.keyCode === 105){
+//                 //     this.numberDisplay('9');
+//                 // }else if (key.keyCode === 104){
+//                 //     this.numberDisplay('8');
+//                 // }else if (key.keyCode === 8){
+//                 //     this.backspace();
+//                 // }else if (key.keyCode === 110){
+//                 //     this.numberDisplay('.');
+//                 // }else if (key.keyCode === 111){
+//                 //     this.appendReset('/');
+//                 // }else if (key.keyCode === 106){
+//                 //     this.appendReset('*');
+//                 // }else if (key.keyCode === 109){
+//                 //     this.appendReset('-');
+//                 // }else if (key.keycode === 32){
+//                 //     this.clearEntry();
+//                 // }else if (key.keycode === 96 || key.keycode === 45){
+//                 //     this.numberDisplay('0');
+//                 }else {
+//                     this.appendReset('+');
+//                 }
+//         },false);
+        
+//     };
